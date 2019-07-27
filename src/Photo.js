5f77a3f61d425ec309e01542490b10dc44b8b029
bishalDores <lazarisalive@gmail.com>
2019-07-27 15:34:45 +0600

    bleeded

--- src/Photo.js
+++ src/Photo.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import downloadIcon from './download.svg'
 
 class Photo extends Component{
     state = {
@@ -19,9 +20,31 @@ class Photo extends Component{
             })
     }
     render(){
-        console.log(this.state.photo)
-        return(
-            <h3>Single Photo</h3>
+        console.log(this.state.photo);
+        var photo = this.state.photo;
+        return (
+            <div>
+
+                {photo.description ? <h2 className="text-center">{photo.description}</h2> : ''}
+                <div className="photo-single-wrapper">
+                    <div className="photo-single-info">
+
+
+
+                        <ul>
+                            <li><label htmlFor="uplaodedd_by">Uploaded by</label> {photo.user && photo.user.first_name} {photo.user && photo.user.last_name}</li>
+
+                            {photo.updated_at ? <li><label htmlFor="upload_date">Upload date</label> {photo.updated_at}</li> : ''}
+
+                            <li><label htmlFor="camera_model">Camera model</label> {photo.exif && photo.exif.model}</li>
+
+                        </ul>
+
+                        <a target="_blank" rel="noopener noreferrer" href={photo.links && photo.links.download} download>Download <img src={downloadIcon} alt="download"/></a>
+                    </div>
+                    <div className="photo-img-n"><img src={photo.urls && photo.urls.full} alt=""/> </div>
+                </div>
+            </div>
         )
     }
 };
